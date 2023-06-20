64bfdabcd79e075ba2a2930b3b843ba058455eb4
ridleykesecker <119704217+ridleykesecker@users.noreply.github.com>
2023-06-20 14:53:15 -0400

    implemented movement and camera boundaries

--- western/pdentrance.gd
+++ western/pdentrance.gd
@@ -11,7 +11,7 @@ func _process(delta):
 	pass
 
 func _on_level_end_zone_body_entered(body):
-	if Input.is_action_pressed("ui_click"):
+	if Input.is_action_pressed("ui_up"):
 		get_tree().change_scene_to_file("res://sheriffs_office_inside.tscn")
 	
 
