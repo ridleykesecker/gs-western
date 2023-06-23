5430f3c641a552b627ee8e46d4bdaa26497ff720
ridleykesecker <119704217+ridleykesecker@users.noreply.github.com>
2023-06-23 10:51:37 -0400

    FGDSFGDSFGD

---
+++ western/keygoaway.gd
@@ -0,0 +1,8 @@
+extends Sprite2D
+
+
+func _process(delta):
+	func tree_entered():
+		queue_free()
+		
+		
--- western/town_outside.gd
+++ western/town_outside.gd
@@ -1,4 +1,14 @@
 extends Node2D
 
-var have_key_1 = true
+var have_key_1 = false
+var have_key_2  = false
 
+@onready var key_one = $key1
+@onready var key_two = $key2
+
+func _process(delta):
+	if key_one == null:
+		have_key_1 = true
+	if key_two == null:
+		have_key_2 = true
+	
